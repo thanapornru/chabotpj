e8fd3f88f42f1410c85c4414e085e5b7d23ff62b
thanapornru <kanjana_135@hotmail.com>
2019-04-02 14:43:20 +0700

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 "# chabotpj" 
+"# linechatbot" 
--- app.py
+++ app.py
@@ -4,7 +4,8 @@ import requests
 
 # ตรง YOURSECRETKEY ต้องนำมาใส่เองครับจะกล่าวถึงในขั้นตอนต่อๆ ไป
 global LINE_API_KEY
-LINE_API_KEY = 'Bearer PlbZSSvpwn72lFKK1SmeA/HrXJcrPrzA3yGRbjnf79meRyYAXxpcnVBBwvMt1sjO5KOuDAOSV/GtEsX9eL63P1k53Zuo3bVyErr3MqImIuSTpQDg90dBcJR1CDQGS6kpTLv3zDJcyXcOT1BFUwc4agdB04t89/1O/w1cDnyilFU='
+# ห้ามลบคำว่า Bearer ออกนะครับเมื่อนำ access token มาใส่
+LINE_API_KEY = 'Bearer Yt+cFnKVPyfwo0Ji7SgxerlZrFMtIdJFHHK+TIpUU4U1jcfhcEQrmV4h+P4F90YeH0gWIMlUDrFfou9tenDDjlGhA5iomBW80TbRc5SMv+OfKaMqfhcuPNK0FXGD6boCSyeXaijClULPBBneiKWeRwdB04t89/1O/w1cDnyilFU='
 
 app = Flask(__name__)
  
@@ -15,20 +16,59 @@ def index():
 
 def bot():
     # ข้อความที่ต้องการส่งกลับ
-    replyStack = list()
+    replyQueue = list()
    
     # ข้อความที่ได้รับมา
     msg_in_json = request.get_json()
     msg_in_string = json.dumps(msg_in_json)
-    
+    text = msg_in_json["events"][0]['message']['text'].lower().strip()
+    print(msg_in_json["events"][0]['message']['text'].lower().strip())
     # Token สำหรับตอบกลับ (จำเป็นต้องใช้ในการตอบกลับ)
     replyToken = msg_in_json["events"][0]['replyToken']
-
-    # ทดลอง Echo ข้อความกลับไปในรูปแบบที่ส่งไป-มา (แบบ json)
-    replyStack.append("ajarn prayat")
-    reply(replyToken, replyStack[:5])
     
-    return 'OK',200
+    # ส่วนนี้ดึงข้อมูลพื้นฐานออกมาจาก json (เผื่อ)
+    userID =  msg_in_json["events"][0]['source']['userId']
+    msgType =  msg_in_json["events"][0]['message']['type']
+    
+    # ตรวจสอบว่า ที่ส่งเข้ามาเป็น text รึป่าว (อาจเป็น รูป, location อะไรแบบนี้ได้ครับ)
+    # แต่ก็สามารถประมวลผลข้อมูลประเภทอื่นได้นะครับ
+    # เช่น ถ้าส่งมาเป็น location ทำการดึง lat long ออกมาทำบางอย่าง เป็นต้น
+    if msgType != 'text':
+        reply(replyToken, ['Only text is allowed.'])
+        return 'OK',200
+    
+    # ตรงนี้ต้องแน่ใจว่า msgType เป็นประเภท text ถึงเรียกได้ครับ 
+    # lower เพื่อให้เป็นตัวพิมพ์เล็ก strip เพื่อนำช่องว่างหัวท้ายออก ครับ
+    if(text == "how are you"):
+         print('in if')
+         replyQueue.append("I am Fat")
+         replyQueue.append("How about you")
+         reply(replyToken, replyQueue[:5])
+         return 'OK', 200
+
+    # ตัวอย่างการทำให้ bot ถาม-ตอบได้ แบบ exact match
+    # response_dict = {'สวัสดี':'สวัสดีครับ'}
+    # if text in response_dict:
+    #     replyQueue.append(reponse_dict[text])
+    # else:
+    #     replyQueue.append('ไม่รู้ว่าจะตอบอะไรดี TT')
+       
+    # ตัวอย่างการทำให้ bot ถาม-ตอบได้ แบบ non-exact match
+    # โดยที่มี method ชื่อ find_closest_sentence ที่ใช้การเปรียบเทียบประโยค
+    # เพื่อค้นหาประโยคที่ใกล้เคียงที่สุด อาจใช้เรื่องของ word embedding มาใช้งานได้ครับ
+    # simple sentence embeddings --> https://openreview.net/pdf?id=SyK00v5xx
+    # response_dict = {'สวัสดี':'สวัสดีครับ'}
+    # closest = find_closest_sentence(response_dict, text)
+    # replyQueue.append(reponse_dict[closest])
+   
+    # ตอบข้อความ "นี่คือรูปแบบข้อความที่รับส่ง" กลับไป
+    replyQueue.append('นี่คือรูปแบบข้อความที่รับส่ง')
+    
+    # ทดลอง Echo ข้อความกลับไปในรูปแบบที่ส่งไปมา (แบบ json)
+    replyQueue.append(msg_in_string)
+    reply(replyToken, replyQueue[:5])
+    
+    return 'OK', 200
  
 def reply(replyToken, textList):
     # Method สำหรับตอบกลับข้อความประเภท text กลับครับ เขียนแบบนี้เลยก็ได้ครับ
