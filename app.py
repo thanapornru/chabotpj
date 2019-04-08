437ee2ff63725d67ee8ced8fbea7261c23212eba
thanapornru <kanjana_135@hotmail.com>
2019-04-08 22:35:46 +0700

    first commit

--- app.py
+++ app.py
@@ -45,6 +45,12 @@ def bot():
          replyQueue.append("เป็นอะไรมาคะ")
          reply(replyToken, replyQueue[:5])
          return 'OK', 200 
+         
+    response_dict = {'สวัสดี':'สวัสดีครับ'}
+    if text in response_dict:
+         replyQueue.append(reponse_dict[text])
+    else:
+         replyQueue.append('ไม่รู้ว่าจะตอบอะไรดี TT')
 
     if(text == "ปวดหัวค่ะ"):
          print('in if')
