c9fa5b105ca1f22ab842b6820204b6d62ca1c343
thanapornru <kanjana_135@hotmail.com>
2019-04-09 01:37:42 +0700

    first commit

--- app.py
+++ app.py
@@ -75,21 +75,7 @@ def bot():
          reply(replyToken, replyQueue[:200])
          return 'OK', 200
 
-    if(text <="5805002440"):
-         print('in if')
-         replyQueue.append("31 มีนาคม 2562")
-         replyQueue.append("มีความต้องการอยากทราบรหัสอื่นเพิ่มเติมอีกไหมคะ?")    
-         reply(replyToken, replyQueue[:200])
-         return 'OK', 200
-
-    if(text >="5805002441"):
-         print('in if')
-         replyQueue.append("30 มีนาคม 2562")
-         replyQueue.append("มีความต้องการอยากทราบรหัสอื่นเพิ่มเติมอีกไหมคะ?")    
-         reply(replyToken, replyQueue[:200])
-         return 'OK', 200
-
-    if(text <="6005004640"):
+    if(text >="5805002440"):
          print('in if')
          replyQueue.append("30 มีนาคม 2562")
          replyQueue.append("มีความต้องการอยากทราบรหัสอื่นเพิ่มเติมอีกไหมคะ?")    
